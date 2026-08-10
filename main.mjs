b67a9731595b9e339e1186e12849ef50a24ce620
Nate <52763514+LlamaWhisp3r3r@users.noreply.github.com>
2026-08-10 18:39:07 -0400

    Add tour start after character creation.

--- main.mjs
+++ main.mjs
@@ -14,39 +14,56 @@ async function userPrompt(message, html, context) {
         console.log("User is GM. Do not show.");
         return;
     }
+    console.log("Checking if I should show Prompt...");
     if ( showPrompt && count == 1 ) {
         console.log("New User logged in...");
-        await user.setFlag("new-player-orientation", "showPrompt", false)
-        console.log("Unset showPrompt flag")
+        console.log("Unset showPrompt flag");
         const proceed = await foundry.applications.api.DialogV2.confirm({
             content: "Looks like you haven't been given the scoop. Want to get to creating a character?",
             rejectClose: false,
             modal: true
         });
-        console.log("Displayed dialog")
-        if ( proceed ) HEROMANCER.api.openWizard();;
-        console.log("Tour done")
+        console.log("Displayed dialog");
+        if ( proceed ) HEROMANCER.api.openWizard();
+        console.log("Tour done");
     }
 }
 
 function userLogin(user, connected) {
-    console.log("User login detected...")
+    console.log("User login detected...");
     if (connected && !user.isGM) {
-        const userFlags = user.flags
+        const userFlags = user.flags;
         if ("new-player-orientation" in user.flags) {
             console.log("The flag is here");
             console.log(userFlags["new-player-orientation"]);
             if ("showPrompt" in userFlags["new-player-orientation"]) return;
         };
-        console.log(user)
-        user.setFlag("new-player-orientation", "showPrompt", true)
+        console.log(user);
+        user.setFlag("new-player-orientation", "showPrompt", true);
         ChatMessage.create({
         content: messageContent,
         whisper: [user.id]
         });
+        console.log("Set showPrompt...");
     }
     console.log(userLogon);
 }
 
+async function tourStart(document, options, userId) {
+    const user = game.users.get(userId);
+    const showPrompt = user.getFlag("new-player-orientation", "showPrompt");
+    if ( options.documentName === "Actor" && showPrompt && !user.isGM) {
+        const proceed = await foundry.applications.api.DialogV2.confirm({
+            content: "Great job on creating a character. Would you like a tour of Foundry?",
+            rejectClose: false,
+            modal: true
+        });
+        if ( proceed ) game.tours.get('core.uiOverview').start();
+        await user.setFlag("new-player-orientation", "showPrompt", false);
+        console.log("Set flag to false.")
+    }
+}
+
 Hooks.on('userConnected', userLogin);
 Hooks.on('renderChatMessageHTML', userPrompt);
+Hooks.on('createActor', tourStart);
