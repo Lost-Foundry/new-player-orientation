d77843eabc3bed214d4328f63a79f41a0ad59a38
Nate <52763514+LlamaWhisp3r3r@users.noreply.github.com>
2026-08-10 14:44:54 -0400

    Ad main scripts

---
+++ main.mjs
@@ -0,0 +1,52 @@
+Hooks.once("init", () => {
+    console.log("New Player Orientation module is initialized...");
+})
+
+let userLogon = [];
+let count = 0;
+const messageContent = `<h1>Welcome to Foundry. Please follow the tour</h1>`
+
+async function userPrompt(message, html, context) {
+    count += 1;
+    const user = context.user;
+    const showPrompt = user.getFlag("new-player-orientation", "showPrompt");
+    if (user.isGM) {
+        console.log("User is GM. Do not show.");
+        return;
+    }
+    if ( showPrompt && count == 1 ) {
+        console.log("New User logged in...");
+        await user.setFlag("new-player-orientation", "showPrompt", false)
+        console.log("Unset showPrompt flag")
+        const proceed = await foundry.applications.api.DialogV2.confirm({
+            content: "Looks like you haven't been given the scoop. Want to get to creating a character?",
+            rejectClose: false,
+            modal: true
+        });
+        console.log("Displayed dialog")
+        if ( proceed ) HEROMANCER.api.openWizard();;
+        console.log("Tour done")
+    }
+}
+
+function userLogin(user, connected) {
+    console.log("User login detected...")
+    if (connected && !user.isGM) {
+        const userFlags = user.flags
+        if ("new-player-orientation" in user.flags) {
+            console.log("The flag is here");
+            console.log(userFlags["new-player-orientation"]);
+            if ("showPrompt" in userFlags["new-player-orientation"]) return;
+        };
+        console.log(user)
+        user.setFlag("new-player-orientation", "showPrompt", true)
+        ChatMessage.create({
+        content: messageContent,
+        whisper: [user.id]
+        });
+    }
+    console.log(userLogon);
+}
+
+Hooks.on('userConnected', userLogin);
+Hooks.on('renderChatMessageHTML', userPrompt);
